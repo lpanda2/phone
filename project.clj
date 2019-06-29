02935bf81a1ae77e6d2ebbed4d2ecd5409ff8ba2
Lipsa Panda <lpanda@Lipsas-MBP.fios-router.home>
2019-06-29 16:41:58 -0400

    help

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject phone "0.1.0-SNAPSHOT"
-  :description "an app that tells you whos callin"
+  :description "an app that tells you whos calling"
   :url "http://localhost/phone"
 
   :source-paths ["src" "src/phone"]

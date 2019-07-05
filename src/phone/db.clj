9e068f675ae9da5c390972e19dd87a88b5de8250
Lipsa Panda <lpanda@Lipsas-MBP.fios-router.home>
2019-07-04 23:05:15 -0400

    loads in data on app load

--- src/phone/db.clj
+++ src/phone/db.clj
@@ -2,23 +2,21 @@
   (:require [phone.layout :refer [say]]
             [mount.core :refer [defstate]]
             [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [libphonenumber.core :refer [parse-phone example-phone]]))
 
-(defonce db {})
-
-
-(defn csv-data->maps [csv-data]
+(defn ->maps [csv-data]
   (map zipmap (repeat [:phone :context :caller-id]) csv-data))
 
-
+(defn ->clean [m]
+  (update m :phone #(-> % (parse-phone "US") second :e164)))
 
 (defn load-data-into-db []
   (let [resource (io/resource "callerid-data.csv")
         csv (with-open [reader (io/reader resource)]
               (doall (csv/read-csv reader)))
-        data (csv-data->maps csv)]
-
-    ))
+        data (->> csv ->maps (map ->clean) (group-by :phone))]
+    data))
 
 (defstate db
   :start (load-data-into-db))
